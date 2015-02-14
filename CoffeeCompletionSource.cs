f1631152dee6c443ab4a00b4fe2f00651dfc7e8d
qdoop Project <github@qdoop.com>
2015-02-14 18:36:31 +0200

    release_150214_
    Enter comething here

---
+++ CoffeeCompletionSource.cs
@@ -0,0 +1,150 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Windows.Media;
+using Microsoft.VisualStudio.Language.Intellisense;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Utilities;
+using Microsoft.Web.Editor;
+using Intel = Microsoft.VisualStudio.Language.Intellisense;
+
+namespace qdoop.EditorExtensions.CoffeeScript
+{
+    [Export(typeof(ICompletionSourceProvider))]
+    [ContentType("CoffeeScript")]
+    [Name("CoffeeScriptCompletion")]
+    class CoffeeCompletionSourceProvider : ICompletionSourceProvider
+    {
+        public ICompletionSource TryCreateCompletionSource(ITextBuffer textBuffer)
+        {
+            return new CoffeeCompletionSource(textBuffer);
+        }
+    }
+
+    class CoffeeCompletionSource : ICompletionSource
+    {
+        private ITextBuffer _buffer;
+        private bool _disposed = false;
+        private static ImageSource _glyph = GlyphService.GetGlyph(StandardGlyphGroup.GlyphGroupVariable, StandardGlyphItem.GlyphItemPublic);
+
+        public CoffeeCompletionSource(ITextBuffer buffer)
+        {
+            _buffer = buffer;
+        }
+
+        public void AugmentCompletionSession(ICompletionSession session, IList<CompletionSet> completionSets)
+        {
+            if (_disposed)
+                return;
+
+            ITextSnapshot snapshot = _buffer.CurrentSnapshot;
+            var triggerPoint = session.GetTriggerPoint(snapshot);
+            if (triggerPoint == null)
+                return;
+
+            List<Intel.Completion> completions = new List<Intel.Completion>();
+            var hset = new HashSet<string>() { 
+                "require('')", 
+                "module.exports", 
+                "console.log ", 
+                
+                "()->" ,
+            };
+
+
+            var kwds =new HashSet<string>() {
+                                        "and", 
+ 									   "break", 
+ 									   "by", 
+									   "catch", 
+ 									   "class", 
+ 									   "continue", 
+                                        "debugger", 
+                                        "delete", 
+ 									   "do", 
+ 									   "else", 
+ 									   "extends", 
+ 									   "false", 
+ 									   "finally", 
+ 									   "for", 
+ 									   "if", 
+ 									   "in", 
+                                        "instanceof", 
+ 									   "is", 
+ 									   "isnt", 
+                                        "loop", 
+ 									   "new", 
+ 									   "no", 
+ 									   "not", 
+ 									   "null", 
+ 									   "of", 
+ 									   "off", 
+ 									   "on", 
+ 									   "or", 
+ 									   "return", 
+ 									   "super", 
+ 									   "switch", 
+ 									   "then", 
+ 									   "this", 
+ 									   "throw", 
+ 									   "true", 
+ 									   "try", 
+                                        "typeof", 
+ 									   "undefined", 
+                                        "unless", 
+ 									   "until", 
+ 									   "when", 
+ 									   "while", 
+ 									   "yes",};
+
+
+            var txt = snapshot.GetText();
+            var tks = txt.Split(new System.Char[] { ' ', '@', '=', '[', ']','{','}', '(',')', '+', '-', '/', '*', ',', '.', ':', '\t', '\r', '\n' });
+    
+            
+            foreach (string item in tks)
+            {
+                if ("" != item.Trim() && 1<item.Length) hset.Add(item);
+            }
+
+            foreach (string item in kwds)
+            {
+                hset.Add(item);
+            }
+
+            foreach (string item in hset)
+            {
+                completions.Add(new Intel.Completion(item, item, null, _glyph, item));
+            }
+
+
+            var line = triggerPoint.Value.GetContainingLine();
+            SnapshotPoint start = triggerPoint.Value;
+            string text = line.GetText();
+
+            //int index = text.IndexOf(':');
+            //int hash = text.IndexOf('#');
+            //if (hash > -1 && hash < triggerPoint.Value.Position || (index > -1 && (start - line.Start.Position) > index))
+            //    return;
+
+            ///if (start <= line.Start) return;
+
+            while ( start > line.Start
+                && !char.IsWhiteSpace((start - 1).GetChar()) 
+                && -1 == "`'#=+-*/,.[]{}()<>?$%^&@!~\"\t\r\n ".IndexOf((start - 1).GetChar())
+                )
+            {
+                start -= 1;
+            }
+
+            var applicableTo = snapshot.CreateTrackingSpan(new SnapshotSpan(start, triggerPoint.Value), SpanTrackingMode.EdgeInclusive);
+
+            completionSets.Add(new CompletionSet("All", "All", applicableTo, completions, Enumerable.Empty<Intel.Completion>()));
+        }
+
+        public void Dispose()
+        {
+            _disposed = true;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# xcoffee
-VisualStudio CoffeeScript Extensions
+[xcoffee](https://github.com/qdoop/xcoffee) for Visual Studio 2013
+=================
+
+[xcoffee](https://github.com/qdoop/xcoffee) extends Visual Studio adding autocomplete Intellisense support for CoffeeScript. 
+
+If you ever write CoffeeScript, will make your life as a developer easier. 
+
+This is for all Web developers using Visual Studio.
+
+Inspired from [Web Essentials](http://vswebessentials.com/). Thank you so much!
+
+
+##Getting started
+To contribute to this project, you'll need to do a few things first:
+
+ 1. Fork the project on GitHub
+ 1. Clone it to your computer
+ 1. Install the [Visual Studio 2013 SDK](http://www.microsoft.com/visualstudio/eng/downloads#d-vs-sdk).
+ 1. Open the solution in VS2013.
+
+To install your local fork into your main VS instance, you will first need to open `Source.extension.vsixmanifest` and bump the version number to make it overwrite the (presumably) already-installed production copy. (alternatively, just uninstall `xcoffee` from within VS first)
+
+You can then build the project, then double-click the VSIX file from the bin folder to install it in Visual Studio.
+
+
+##Useful Links
+ - [Web Essentials on Github](https://github.com/madskristensen/WebEssentials2013)
+ - [Getting started with Visual Studio extension development](http://blog.slaks.net/2013-10-18/extending-visual-studio-part-1-getting-started/)
+ - [Inside the Visual Studio editor](http://msdn.microsoft.com/en-us/library/vstudio/dd885240.aspx)
+ - [Extending the Visual Studio editor](http://msdn.microsoft.com/en-us/library/vstudio/dd885244.aspx)
